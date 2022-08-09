2b7a8fd7def167c86489310e875b622a647a8d83
Robert_Bohdziewicz <robert_bohdziewicz@epam.com>
2022-08-09 16:37:32 +0200

    Initial commit

---
+++ src/main/java/com/tipico/jenkinsTests/JenkinsTestsApplication.java
@@ -0,0 +1,13 @@
+package com.tipico.jenkinsTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tipico/jenkinsTests/JenkinsTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tipico.jenkinsTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
